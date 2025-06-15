40381659cbb7fc32b0789f0f404055f470a788de
Ilxom <ilxomkarimjonov22@gmail.com>
2025-06-15 16:45:03 +0500

    Add ef core extensions

---
+++ src/backend/CashBook.Persistence/Extensions/EfCoreExtensions.cs
@@ -0,0 +1,28 @@
+using CashBook.Domain.Common.Entities;
+using CashBook.Domain.Common.Queries;
+using Microsoft.EntityFrameworkCore;
+
+namespace CashBook.Persistence.Extensions;
+
+public static class EfCoreExtensions
+{
+    private static IQueryable<TSource> ApplyTrackingMode<TSource>(
+        this IQueryable<TSource> source,
+        QueryTrackingMode trackingMode) where TSource : class, IEntity
+    {
+        return trackingMode switch
+        {
+            QueryTrackingMode.AsTracking => source,
+            QueryTrackingMode.AsNoTracking => source.AsNoTracking(),
+            QueryTrackingMode.AsNoTrackingWithIdentityResolution => source.AsNoTrackingWithIdentityResolution(),
+            _ => source
+        };
+    }
+
+    public static IQueryable<TSource> ApplyQueryOptions<TSource>(
+        this IQueryable<TSource> source,
+        QueryOptions queryOptions) where TSource : class, IEntity
+    {
+        return source.ApplyTrackingMode(queryOptions.TrackingMode);
+    }
+}
