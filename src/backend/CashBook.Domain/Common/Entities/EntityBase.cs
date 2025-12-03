a3fe9836d88dd68c572a121658099a2221d3e9d4
ilxom-22 <ilxomkarimjonov22@gmail.com>
2025-12-03 11:42:38 +0500

    Enhance entity base - make id as private set, so that EF core could set it

--- src/backend/CashBook.Domain/Common/Entities/EntityBase.cs
+++ src/backend/CashBook.Domain/Common/Entities/EntityBase.cs
@@ -1,23 +1,19 @@
+using CashBook.Domain.Common.BusinessRule;
 using CashBook.Domain.Common.Events;
 
 namespace CashBook.Domain.Common.Entities;
 
 public abstract class EntityBase
 {
-    public Guid Id { get; } = Guid.NewGuid();
+    public Guid Id { get; private set; } = Guid.NewGuid();
 
     private readonly List<EventBase> _domainEvents = new();
     
     public IReadOnlyCollection<EventBase> DomainEvents => _domainEvents.AsReadOnly();
 
-    public void AddDomainEvent(EventBase domainEvent)
+    protected void AddDomainEvent(EventBase domainEvent)
     {
         _domainEvents.Add(domainEvent);
-    }
-
-    public void RemoveDomainEvent(EventBase domainEvent)
-    {
-        _domainEvents.Remove(domainEvent);
     }
 
     public void ClearDomainEvents()
@@ -40,4 +36,12 @@ public abstract class EntityBase
     }
 
     public override int GetHashCode() => Id.GetHashCode();
+    
+    protected void CheckRule(IBusinessRule rule)
+    {
+        if (rule.IsBroken())
+        {
+            throw new BusinessRuleValidationException(rule);
+        }
+    }
 }
